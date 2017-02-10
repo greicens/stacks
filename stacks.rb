12b1db850f6b17e6701dc3b74e26ba04eea6fd2e
greicens <gsilva@mail.ccsf.edu>
2017-02-09 21:20:39 -0800

    ok

--- stacks.rb
+++ stacks.rb
@@ -4,18 +4,28 @@ class Stack
   attr_reader :top, :contents
 
   def initialize(top=nil)
+        @contents = SinglyLinkedList.new(top)
+        @top = @contents.head
   end
 
   def inspect
+    @contents.inspect
   end
 
   def push (element)
+    @contents.insert_at_start(element)
+    @top = @contents.head
   end
 
   def pop
+    element_deleted = @top
+    @contents.remove_from_start
+    @top = @contents.head
+    element_deleted.data
   end
 
   def empty?
+    @contents.empty?
   end
 
 end
